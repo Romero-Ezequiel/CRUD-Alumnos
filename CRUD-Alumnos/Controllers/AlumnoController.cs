895bddef4af9eff92c067f6076ecf926ffd76044
Romero Ezequiel <romerorezequiel@gmail.com>
2022-01-09 00:02:06 -0300

    Realice algunos cambios

--- CRUD-Alumnos/Controllers/AlumnoController.cs
+++ CRUD-Alumnos/Controllers/AlumnoController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CRUD_Alumnos.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,37 @@ namespace CRUD_Alumnos.Controllers
         // GET: Alumno
         public ActionResult Index()
         {
+
+            ModelAlumnos db = new ModelAlumnos();
+
+            db.Alumno.ToList();
+
+           // List<Alumno> lista = db.Alumno.Where(a => a.Edad > 18).ToList();  
+            // db.Alumno.Where(a => a.Edad > 18).ToList();
+
+          //  return View(lista);
+            return View(db.Alumno.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Agregar()
+        {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Agregar(Alumno a)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            
+
+            
+
+            return View();
+        }
+
+
+
     }
 }
---
+++ CRUD-Alumnos/Models/Entity/Alumno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using CRUD_Alumnos.Models;
+
+
+namespace CRUD_Alumnos.Models.Entity
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public int Edad { get; set; }
+        public string Sexo { get; set; }
+        public DateTime FechaRegistro { get; set; }
+
+
+    }
+
+
+}
