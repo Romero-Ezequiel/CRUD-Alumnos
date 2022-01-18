18a905be8995451c5604ea34ad47114fd25bde2c
Romero Ezequiel <romerorezequiel@gmail.com>
2022-01-18 20:13:30 -0300

    Realice un cambio

--- CRUD-Alumnos/Global.asax.cs
+++ CRUD-Alumnos/Global.asax.cs
@@ -12,7 +12,9 @@ namespace CRUD_Alumnos
     //ACA se registran los eventos cuando arranca el sistema,
     //cuando hay un error, cuando se para el servidor
     public class MvcApplication : System.Web.HttpApplication
-    {
+    { 
+
+        //Esto se ejecuta cada vez que la aplicación inicia
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
